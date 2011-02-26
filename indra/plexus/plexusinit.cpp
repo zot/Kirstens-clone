74f517c9879ee7b352e27b396f1b5620aa210f59
Bill Burdick <bill.burdick@gmail.com>
2011-02-26 17:15:34 -0600

    starting plexus mods

---
+++ indra/plexus/plexusinit.cpp
@@ -0,0 +1,17 @@
+/*
+ * plexusinit.cpp
+ *
+ *  Created on: Feb 26, 2011
+ *      Author: bill
+ */
+
+#include <stdio.h>
+#include "plexusinit.h"
+
+void beep() {
+	printf("BEEP\n");
+}
+
+namespace plexus {
+
+};
---
+++ indra/plexus/plexusinit.h
@@ -0,0 +1,19 @@
+/*
+ * plexusinit.h
+ *
+ *  Created on: Feb 26, 2011
+ *      Author: bill
+ */
+
+#ifndef PLEXUSINIT_H_
+#define PLEXUSINIT_H_
+
+extern "C" {
+	void plexusbeep();
+}
+
+namespace plexus {
+	void init();
+};
+
+#endif /* PLEXUSINIT_H_ */
